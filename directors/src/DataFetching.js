0476473341d7d8017791ac587f1f8f475f32ef2b
joshadams1 <joshadams10@gmail.com>
2020-06-15 21:09:42 -0600

    Add stringify to async in order to access properties of object

--- directors/src/DataFetching.js
+++ directors/src/DataFetching.js
@@ -1,18 +1,15 @@
 import React, { useState, useEffect } from 'react';
 
-const DataFetching = (props) => {
+const DataFetching = () => {
     const [hasError, setErrors] = useState(false);
-    const [data, setData] = useState();
-    const id = [7467, 78160, 136495, 137427, 21684];
-    const newId = [];
+    const [data, setData] = useState({});
 
     async function fetchData() {
-        /* CORS error results from the request. Plugin added to temporarily fix it in local enviornment. 
-        See this to fix it when ready to deploy ---> https://medium.com/@dtkatz/3-ways-to-fix-the-cors-error-and-how-access-control-allow-origin-works-d97d55946d9 */
         const res = await fetch(`https://api.themoviedb.org/3/person/21684?api_key=c4da3b1a9df6fc0121e72f785daa41a8&language=en-US`);
         res
             .json()
-            .then(res => setData(res.biography))
+            .then(res => setData(res))
+            // .then(setData(JSON.stringify(res)))
             .catch(err => setErrors(err))
     }
 
@@ -21,9 +18,9 @@ const DataFetching = (props) => {
     }, []);
 
     return (
-        < div >
-            <h1>{id.map(ids => fetchData(ids))}</h1>
-        </div >
+        <div>
+            <h1>{data.biography}</h1>
+        </div>
     );
 };
 
