5a171396bbc5bf1184cf52b5e1cbf11df054ed19
joshadams1 <joshadams10@gmail.com>
2020-06-24 16:29:03 -0600

    Initial commit

--- directors/src/App.js
+++ directors/src/App.js
@@ -1,8 +1,7 @@
 import React, { useState, useEffect } from 'react';
-import './App.css';
-import DataFetching from './DataFetching';
-import directors from './directors';
+import './App.scss';
 import DirectorNavigator from './DirectorNavigator';
+import Button from '@material-ui/core/Button';
 
 function App() {
   return (
